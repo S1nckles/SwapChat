be6c651c4716e21141fe0185f29cacba3c2d3a95
Andrey <ilynych0810@gmail.com>
2024-09-12 16:46:02 +0200

    Added routing and structuring of folders and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { Route, Routes } from '../node_modules/react-router-dom/dist/index';
 import './global.css'
+import Home from './_root/pages/Home';
+import SigninForm from "./_auth/forms/SinginForm";
+import SignupForm from "./_auth/forms/SinginForm";
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="flex h-screen">
+      {/* Public Routes */}
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sig-in" element={<SigninForm />}/>
+          <Route path="/sig-up" element={<SignupForm />}/>
+        </Route>
+      </Routes>
+
+      {/* Private Routes */}
+      <Routes>
+        <Route element={<RootLayout/>}>
+          <Route index element={<Home />}/>
+        </Route>
+      </Routes>
+
+    </main>
   );
 };
 
