e86e1f339f970a8a8d6e354f0ae6f5c51ee42f55
Andrey <ilynych0810@gmail.com>
2024-10-10 10:07:01 +0200

    Adding the function of creating posts

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,6 +1,8 @@
-import { INewUser } from '@/types/index';
+import { INewPost, INewUser } from '@/types/index';
+import CreatePost from '@/_root/pages/CreatePost';
 import { useQuery, useMutation, useQueryClient, useInfiniteQuery} from "@tanstack/react-query";
-import { CreateUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
+import { createPost, CreateUserAccount, getRecentPosts, signInAccount, signOutAccount } from '../appwrite/api';
+import { QUERY_KEYS } from './queryKeys';
 
 export const useCreateUserAccount = () => {
   return useMutation({
@@ -20,3 +22,23 @@ export const useSignOutAccount = () => {
     mutationFn: signOutAccount,
   });
 };
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    }, 
+  });
+};
+
+export const useGetRecentPosts = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts,
+  })
+}
