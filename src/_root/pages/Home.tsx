d470907388923c1177f289bb954ef67791887eca
Andrey <ilynych0810@gmail.com>
2024-10-20 12:20:57 +0200

    View posts on the Home page

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { Models } from "appwrite";
 import { Loader } from "@/components/shared/Loader";
 import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
+import PostCard from "@/components/shared/PostCard";
 
 const Home = () => {
     const { data: posts, isPending: isPostLoading, isError: isErrorPost } = useGetRecentPosts();
 
+    
     return (
         <div className="flex flex-1">
             <div className="home-container">
@@ -12,10 +14,10 @@ const Home = () => {
                     <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
                     {isPostLoading && !posts ? (
                         <Loader />
-                    ) : (
-                        <ul className="flex flex-col flex-1 gap-9 w-full">
+                        ) : (
+                            <ul className="flex flex-col flex-1 gap-9 w-full">
                             {posts?.documents.map((post: Models.Document) => (
-                                <li key={post.$id}>{post.caption}</li>
+                                <PostCard post={post} key={post.caption} />
                             ))}
                         </ul>
                     )}
